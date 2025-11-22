628fb8d418123ddac35127716a388b56fc790a5a
darsankrishna <116716856+darsankrishna@users.noreply.github.com>
2025-11-22 23:57:05 +0530

    Replace Hardhat Account #2 address in whitelistData

--- frontend/constants/whitelistData.js
+++ frontend/constants/whitelistData.js
@@ -1,6 +1,6 @@
 export const whitelistData = [
     { address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", spots: 2 }, // Hardhat Account #0
     { address: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", spots: 2 }, // Hardhat Account #1
-    { address: "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", spots: 2 }, // Hardhat Account #2
+    { address: "0x886e497df7fe63e97821dbfa48b04de5de1d06e9", spots: 2 }, // Hardhat Account #2
     // Add more addresses here
 ];
