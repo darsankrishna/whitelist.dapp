2625d2e33a61f21d9d5a21c587c74603c315c3f5
darsankrishna <darsankrishna12@gmail.com>
2025-11-23 00:06:37 +0530

    modified:   frontend/constants/index.js

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,4 +1,4 @@
 import WhitelistABI from './Whitelist.json';
 
-export const WHITELIST_CONTRACT_ADDRESS = "0x6F280a86640A0C29A8cD5d13d14D376981420554";
+export const WHITELIST_CONTRACT_ADDRESS = "0x33E569c82F15A1ca5BCCE1a0F0355e50e4401893";
 export const abi = WhitelistABI.abi;
